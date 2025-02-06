73bd547bfe90393692257ca82f961a32dd83e4a7
yenyen153 <live871008@gmail.com>
2025-02-06 17:47:13 +0800

    資料結構set

--- main.py
+++ main.py
@@ -40,3 +40,31 @@ debug_mode = os.getenv("DEBUG")
 
 print(f"API Key: {api_key}")
 print(f"Debug Mode: {debug_mode}")
+
+#%%
+## 資料結構 - set
+## set中不允許相同元素存在、可去除重複元素，以jieba斷詞為例
+
+import jieba
+
+text = '今天的天氣很好，我的心情也很好，下班後想去咖啡廳、書局，但時間不夠，只好回家'
+
+text_token = jieba.lcut(text, cut_all=False)
+text_set = set(text_token)  ## 使用set可得出text有那些詞組
+print(text_set)
+print('共有:',len(text_set),'個詞組')
+# >>>{'下班', '，', '但', '時間', '、', '心情', '的', '好', '我', '書局', '只好', '回家', '天氣', '廳', '後', '很', '去', '也', '想', '今天', '不夠', '咖啡'}
+# >>>共有: 22 個詞組
+
+## set是無序的，與list或tuple不同，因此set通常有整合的意義
+## 也可以充作找交集的工具，例如下面sent1、sent2，透過交集找出可能的stop words
+
+sent1 = "syntax tree是一種句法樹狀圖，可以清楚知道支配與被支配的詞，常見的像是NP、VP、TP"
+sent2 = "VP表示動詞片語，基本單元一動詞一受詞、NP表示名詞片語，漢語基本單元是一名詞，以此類推，這些片語組成的圖我們稱syntax tree"
+
+sent1_set = set(jieba.lcut(sent1, cut_all=False))
+sent2_set = set(jieba.lcut(sent2, cut_all=False))
+common_token = sent1_set & sent2_set
+
+print(common_token)
+# >>>{'詞', '的', 'VP', '，', 'syntax', 'tree', ' ', '、', 'NP', '是'}

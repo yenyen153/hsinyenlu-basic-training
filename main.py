edab8c32f0ba1a8d2290ee78375669e5921152fc
yenyen153 <live871008@gmail.com>
2025-02-06 17:38:20 +0800

    執行參數設定與使用原因

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
+#%%
+## 執行參數設定與使用原因
+## 1. 點選右上角專案名稱
+## 2. 找到Edit Configurations
+## 3. 找到script parameters並輸入指定parameters
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+## 設定parameters原因:
+## 雖然使用terminal也可以指定參數，但每次執行都必須重新輸入，當某些參數可以鎖死不需要每次都輸入時，直接在pycharm設定即可
+## 優點在於，使用pycharm鎖死參數，可以避免浪費每次都要重新輸入的時間，若要更改參數時，也可透過pycharm中內部設定做更改，不會直接更改到主程式
 
+## 以下為例
+import argparse
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+parser = argparse.ArgumentParser(description="orders")
 
+parser.add_argument("--food", required=str, help="輸入食物名稱")
+parser.add_argument("--drink", required=str, help="輸入飲料名稱")
+parser.add_argument("--side", required=str, help="輸入附餐名稱")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+args = parser.parse_args()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(f"我要一份:{args.food}、{args.drink}附餐要{args.side}!")
